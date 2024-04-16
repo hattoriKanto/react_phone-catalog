96cbb01065c3745be43ab117b40a4674685e2908
Елена Карнаух <elenakarnauh@MacBook-Pro-Elena.local>
2024-04-17 00:47:40 +0300

    create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => (
+  <div className="footer">
+    <div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <a href="">
+        <p></p>
+      </a>
+      <a href="">
+        <p></p>
+      </a>
+      <a href="">
+        <p></p>
+      </a>
+    </div>
+    <div></div>
+  </div>
+);
+
+export default Footer;
+
