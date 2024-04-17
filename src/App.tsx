476186b5943c2938e65cb15a08db6372c44eada6
Illia <ironme530@gmail.com>
2024-04-17 11:15:03 +0300

    implemented useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import { useEffect } from 'react';
-import { getPhones } from './utils/client';
+import useFetchData from './utils/client';
+import { Phone } from './types/Phone';
 
 function App() {
-  useEffect(() => {
-    getPhones();
-  }, []);
+  const { data, isLoading, error } = useFetchData<Phone>('phones.json');
+
+  !isLoading && console.log(data);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
     <>
--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,11 +1,39 @@
 import axios from 'axios';
-import { Phone } from '../types/Phone';
+import { useState, useEffect } from 'react';
 
-export async function getPhones() {
-  try {
-    const response: Phone[] = (await axios.get('./api/phones.json')).data;
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
+type FetchState<T> = {
+  data: T | null;
+  isLoading: boolean;
+  error: Error | null;
+};
+
+function useFetchData<T>(url: string): FetchState<T> {
+  const BASE_URL = '/react_phone-catalog/public/api/'
+
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<T>(BASE_URL + url);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+          setError(new Error('Failed to fetch data'));
+        } else {
+          setError(new Error('An unknown error occurred'));
+        }
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
 }
+
+export default useFetchData;
