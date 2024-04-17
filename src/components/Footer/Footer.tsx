1cb0cee2e33d774257b5bb432eee0748d23aa940
Елена Карнаух <elenakarnauh@MacBook-Pro-Elena.local>
2024-04-17 11:44:57 +0300

    create assets folder

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,6 @@
 const Footer = () => (
-  <div className="footer">
-    <div>
-      <img src="" alt="" />
-    </div>
-    <div>
-      <a href="">
-        <p></p>
-      </a>
-      <a href="">
-        <p></p>
-      </a>
-      <a href="">
-        <p></p>
-      </a>
-    </div>
-    <div></div>
-  </div>
+  <footer className="footer">
+  </footer>
 );
 
 export default Footer;
