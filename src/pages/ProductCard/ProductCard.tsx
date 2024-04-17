26428256b8dc96d1dd1065a163906385efdcdad1
IvanGritsenko <gritsenkofar2@gmail.com>
2024-04-17 16:51:35 +0300

    Add card layout

---
+++ src/pages/ProductCard/ProductCard.tsx
@@ -0,0 +1,141 @@
+import { useState } from 'react';
+import Card from '@mui/material/Card';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import {
+  Button,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+
+export const ProductCard = () => {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isCart, setIsCart] = useState(false);
+
+  return (
+    <Card
+      sx={{
+        boxSizing: 'border-box',
+        width: 272,
+        height: 506,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: 1,
+        borderColor: '#E2E6E9',
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="50%"
+        image={`img/phones/apple-iphone-7/black/00.webp`}
+        sx={{
+          pt: 4,
+          height: 196,
+          width: 208,
+          objectFit: 'contain',
+          objectPosition: 'center',
+        }}
+      />
+      <CardContent sx={{ m: 1 }}>
+        <Typography variant="h6" component="div">
+          Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
+        </Typography>
+        <Stack direction="row" spacing={2} sx={{ pt: 1, pb: 1 }}>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            $799
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: 'bold',
+              color: '#89939A',
+              textDecoration: 'line-through',
+            }}
+          >
+            $899
+          </Typography>
+        </Stack>
+
+        <Divider />
+
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ justifyContent: 'space-between', pt: 1 }}
+        >
+          <Typography variant="body1" sx={{ color: '#89939A' }}>
+            Screen
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ color: 'black', fontWeight: 'bold' }}
+          >
+            5.8" OLED
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ justifyContent: 'space-between', pt: 0.5 }}
+        >
+          <Typography variant="body1" sx={{ color: '#89939A' }}>
+            Capacity
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ color: 'black', fontWeight: 'bold' }}
+          >
+            64 GB
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ justifyContent: 'space-between', pt: 0.5 }}
+        >
+          <Typography variant="body1" sx={{ color: '#89939A' }}>
+            RAM
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ color: 'black', fontWeight: 'bold' }}
+          >
+            4 GB
+          </Typography>
+        </Stack>
+
+        <Grid container spacing={2} sx={{ pt: 4, alignItems: 'center' }}>
+          <Grid xs={8}>
+            <Button
+              variant={!isCart ? 'contained' : 'outlined'}
+              onClick={() => setIsCart(prev => !prev)}
+            >
+              Add to cart
+            </Button>
+          </Grid>
+
+          <Grid xs={4}>
+            <IconButton
+              sx={{ border: 1, borderColor: '#B4BDC3', color: 'black' }}
+              aria-label="add to favorites"
+              onClick={() => setIsFavorite(prev => !prev)}
+            >
+              {!isFavorite ? (
+                <FavoriteBorderIcon />
+              ) : (
+                <FavoriteIcon sx={{ color: '#476DF4' }} />
+              )}
+            </IconButton>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
