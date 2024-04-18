7735240e8c8114a3d47532dd9de7041316b0092c
Елена Карнаух <elenakarnauh@MacBook-Pro-Elena.local>
2024-04-18 03:19:27 +0300

    completed footer component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Root } from './Root.tsx';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Root />
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
   </React.StrictMode>,
 );
