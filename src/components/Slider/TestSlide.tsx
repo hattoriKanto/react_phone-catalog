5238ffe4b6834895587eff0ca8ec48da95049b8a
Nirvacsh <nir.vlad.zu@gmail.com>
2024-04-22 11:56:37 +0300

    hotfix!!

--- src/components/Slider/TestSlide.tsx
+++ src/components/Slider/TestSlide.tsx
@@ -1,18 +1,24 @@
-import React, { FC } from 'react';
-import { Test } from './Slider.styles.tsx';
+// import React, { FC } from 'react';
 
-type Props = {
-  children?: React.ReactNode
-  backgroundTablet: string
-  backgroundMobile: string
-}
+// type Props = {
+//   children?: React.ReactNode;
+//   backgroundTablet: string;
+//   backgroundMobile: string;
+// };
 
-const TestSlide: FC<Props> = ({ children, backgroundTablet, backgroundMobile }) =>{
-  return(
-    <Test backgroundTablet={backgroundTablet} backgroundMobile={backgroundMobile}>
-      {children}
-    </Test>
-  )
-}
+// const TestSlide: FC<Props> = ({
+//   children,
+//   backgroundTablet,
+//   backgroundMobile,
+// }) => {
+//   return (
+//     <Test
+//       backgroundTablet={backgroundTablet}
+//       backgroundMobile={backgroundMobile}
+//     >
+//       {children}
+//     </Test>
+//   );
+// };
 
-export default TestSlide
+// export default TestSlide;
