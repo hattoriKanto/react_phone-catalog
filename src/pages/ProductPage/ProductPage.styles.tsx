afa94f6c8c17c03c1db9481b93b8562d0424378a
Ihor Bahniuk <hattorikanto@gmail.com>
2024-04-22 19:42:29 +0300

    about section has been added on product page

---
+++ src/pages/ProductPage/ProductPage.styles.tsx
@@ -0,0 +1,19 @@
+import { Stack, styled } from '@mui/material';
+
+export const StyledFlexWrapper = styled(Stack)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+
+  [theme.breakpoints.up('xs')]: {
+    flexDirection: 'column',
+    gap: '56px',
+  },
+
+  [theme.breakpoints.up('sm')]: {
+    gap: '64px',
+  },
+
+  [theme.breakpoints.up('lg')]: {
+    flexDirection: 'row',
+  },
+}));
