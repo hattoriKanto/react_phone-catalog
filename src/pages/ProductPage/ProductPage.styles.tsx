cff049f0d62a45aad8c0b4c71fec912fa506f16d
Ihor Bahniuk <hattorikanto@gmail.com>
2024-04-23 11:44:20 +0300

    1

--- src/pages/ProductPage/ProductPage.styles.tsx
+++ src/pages/ProductPage/ProductPage.styles.tsx
@@ -2,18 +2,20 @@ import { Stack, styled } from '@mui/material';
 
 export const StyledFlexWrapper = styled(Stack)(({ theme }) => ({
   display: 'flex',
-  alignItems: 'center',
 
   [theme.breakpoints.up('xs')]: {
+    marginTop: '56px',
     flexDirection: 'column',
     gap: '56px',
   },
 
   [theme.breakpoints.up('sm')]: {
+    marginTop: '64px',
     gap: '64px',
   },
 
   [theme.breakpoints.up('lg')]: {
+    marginTop: '80px',
     flexDirection: 'row',
   },
 }));
