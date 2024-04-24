df9d0dfa73fd6d87cea0f4369a9b7af114d0a424
Illia <ironme530@gmail.com>
2024-04-24 21:19:47 +0300

    fixed key issue

--- src/components/ChangeColorSizeBlock/ChangeColorSizeBlock.tsx
+++ src/components/ChangeColorSizeBlock/ChangeColorSizeBlock.tsx
@@ -71,7 +71,7 @@ export const ChangeColorSizeBlock: React.FC<Props> = ({
       <Box>
         <OptionsTitle>Available colors</OptionsTitle>
         <Colors>
-          {selectedData?.colorsAvailable.map(color => {
+          {selectedData?.colorsAvailable.map((color, i) => {
             const tempColor =
               ColorsAvailable[color as keyof typeof ColorsAvailable];
             return (
@@ -80,7 +80,7 @@ export const ChangeColorSizeBlock: React.FC<Props> = ({
                   handleChangeColor(color);
                   setSelectedColor(color);
                 }}
-                key={tempColor}
+                key={i}
                 className={selectedColor === color ? 'active' : ''}
               >
                 <Color style={{ backgroundColor: tempColor }} />
