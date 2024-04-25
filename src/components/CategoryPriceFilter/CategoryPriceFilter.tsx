feef662b4fa9c41c3d5d1c5904cd2de308bd489e
Khrystyna Hryts <hi4lan@gmail.com>
2024-04-25 12:38:16 +0300

    price range

--- src/components/CategoryPriceFilter/CategoryPriceFilter.tsx
+++ src/components/CategoryPriceFilter/CategoryPriceFilter.tsx
@@ -7,7 +7,7 @@ type Props = {
   minPriceInCategory: number;
 }
 
-export const PriceRange: React.FC<Props> = ({ minPriceInCategory, maxPriceInCategory }) => {
+export const CategoryPriceFilter: React.FC<Props> = ({ minPriceInCategory, maxPriceInCategory }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [priceRange, setPriceRange] = useState<number[]>([minPriceInCategory, maxPriceInCategory]);
 
