5831ac470b66236cdf48c33f13ba8700e978d91e
Nirvacsh <nir.vlad.zu@gmail.com>
2024-04-26 11:32:02 +0300

    fix

--- src/theme/breakpoints.config.ts
+++ src/theme/breakpoints.config.ts
@@ -2,7 +2,7 @@ export const customBreakpoints = {
   values: {
     xs: 320, // extra-small: Mobile devices
     sm: 640, // small: Tablet devices
-    md: 780, // medium: Desktop devices
+    md: 880, // medium: Desktop devices
     lg: 1200,
     xl: 2560,
   },
